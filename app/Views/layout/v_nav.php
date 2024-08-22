c01b127d06a3c0c6c6d5cf1823d3ae46d7abcbb7
rahmalarasati <larasrahma21@students.amikom.ac.id>
2024-08-22 20:22:36 +0700

    login

--- app/Views/layout/v_nav.php
+++ app/Views/layout/v_nav.php
@@ -1,8 +1,8 @@
 <!-- Collect the nav links, forms, and other content for toggling -->
 <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="<? base_url() ?>">Home <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
+            <li><a href="<? base_url() ?>">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">About</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -4,7 +4,7 @@
             <h1><b>Sistem Informasi Akademik</b></h1>
     </div>
     <div class="col-sm1-12">
-             <img class="img-responsive pad" src="<?= base_url('gambar/akademik.png') ?>"width= "700px" alt="Photo">
+             <img class="img-responsive pad" src="<?= base_url('gambar/akademik.png') ?>"width= "" alt="Photo">
     </div>
 
 </div>
