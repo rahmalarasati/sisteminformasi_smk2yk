dd3328c991751ae2d4f29caaf2cae1a5d303de82
rahmalarasati <larasrahma21@students.amikom.ac.id>
2024-08-27 22:00:08 +0700

    nambahin data siswa

--- app/Views/teknisi/v_datasiswa.php
+++ app/Views/teknisi/v_datasiswa.php
@@ -6,8 +6,8 @@
 </section> 
 
 <div class="row"> 
-    <div class="col-sm1-12">
-    <div class="box box-success box-solid">
+    <div class="col-sm-12">
+    <div class="box box-primary box-solid">
     <div class="box-header with-border">
               <h3 class="box-title"> <?= $title ?> </h3>
               <div class="box-tools pull-right">
@@ -16,16 +16,50 @@
               <!-- /.box-tools -->
             </div>
             <!-- /.box-header -->
-            <div class="box-body">
+            <div class="table-responsive">
             <table id="example1" class="table table-bordered table-striped">           
             <thead>
                 <tr>
                     <th width="50px">No</th>
-                    <th>Data Siswa</th>
-                    <th>Action</th>
+                    <th>NIS</th>
+                    <th>NISN</th>
+                    <th>Nama</th>
+                    <th>Jurusan</th>
+                    <th>Kelas</th>
+                    <th>Email</th>
+                    <th>No. Bukti Pendaftaran</th>
+                    <th>Peminatan</th>
+                    <th>Jenis Kelamin</th>
+                    <th>No. HP</th>
+                    <th>Agama</th>
+                    <th>Tinggi Badan</th>
+                    <th>Berat Badan</th>
+                    <th>Golongan Darah</th>
+                    <th>Hobi</th>
+                    <th>Cita-cita</th>
+                    <th>No. Registrasi Akta Kelahiran</th>
+                    <th>Upload Scan Akta Kelahiran</th>
+                    <th>Sekolah Asal</th>
+                    <th>Alamat Sekolah Asal</th>
+                    <th>Tahun Lulus SMP</th>
+                    <th>No. Seri Ijazah SMP</th>
+                    <th>Tinggal Dengan</th>
+                    <th>Jarak Rumah Ke Sekolah</th>
+                    <th>Transportasi Ke Sekolah</th>
+                    <th>Waktu Tempuh Ke Sekolah</th>
+                    <th>No. Kartu Keluarga</th>
+                    <th>Upload Scan Kartu Keluarga</th>
+                    <th>Anak Ke</th>
+                    <th>Julah Saudara Kandung</th>
+                    <th>Upload KPS/PKH</th>
+                    <th>Apakah Punya KIP</th>
+                    <th>Upload KIP</th>
+                    <th>Prestasi 3 Tahun Terakhir</th>
+                    <th width="150px">Action</th> 
                 </tr>
             </thead>
             <tbody>
+              
             </tbody>
         </table>
             </div>
